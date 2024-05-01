27d4be33b1ee6688ff05d2283b04c877d6f92cd3
Ethan Zang <675483520@qq.com>
2024-05-01 21:54:37 +0800

    [fix] deposit.js code error

--- deposit.js
+++ deposit.js
@@ -8,9 +8,8 @@
  * node deposit.js EAjFK3iWqYdRbCAuDhfCNHo2EMj3S7eg5QrU7DMcNEXD 0x9fE46736679d2D9a65F0992F2272dE9f3c7fa6e0 12345 12345 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80 http://localhost:8545
  */
 
-const ethers = require('ethers');
+const { ethers, utils } = require('ethers');
 const bs58 = require('bs58');
-const utils = require('ethers/utils');
 
 // Check if enough arguments are provided
 if (process.argv.length < 7) {
@@ -26,18 +25,18 @@ const decodedSolanaAddress = bs58.decode(solanaAddress);
 const hexSolanaAddress = utils.hexlify(decodedSolanaAddress);
 
 // Setup Ethereum blockchain connection
-const provider = new ethers.JsonRpcProvider(jsonRpcUrl);
+const provider = new ethers.providers.JsonRpcProvider(jsonRpcUrl);
 const wallet = new ethers.Wallet(privateKey, provider);
 
 // Define the EtherBridge contract interaction
 async function depositToEtherBridge() {
-    const abi = ["function deposit(bytes32,uint256,uint256)"];
+    const abi = ["function deposit(bytes32,uint256,uint256) payable"];
     const contract = new ethers.Contract(etherBridgeAddress, abi, wallet);
 
     try {
         const amountWei = utils.parseUnits(amount, 'gwei');
         const feeWei = utils.parseUnits(fee, 'gwei');
-        const totalWei = amountWei + feeWei;
+        const totalWei = amountWei.add(feeWei);
         const tx = await contract.deposit(hexSolanaAddress, amountWei, feeWei, {
             value: totalWei
         });
